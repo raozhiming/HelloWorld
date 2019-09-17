12b413c56f2e4f93830a02085365ccfbf65b3b0d
raozhiming <raozhiming@elastos.org>
2019-09-17 15:54:38 +0800

    test

--- .ci_scripts/checkCommit.sh
+++ .ci_scripts/checkCommit.sh
@@ -3,30 +3,31 @@
 # skip commit with ci-skip or skip-ci
 # since one day ago
 
-# HOST="$(uname -s)"
+HOST="$(uname -s)"
 
-# case "${HOST}" in
-#     "Darwin")
-#         STRIP_ARG="--strip-components=1"
-#         ;;
-#     "Linux")
-#         STRIP_ARG="--strip-components 1"
-#         ;;
-#     *)
-#         echo "Error: Unsupported platform"
-#         exit 1;;
-# esac
+case "${HOST}" in
+    "Darwin")
+        DATE_ARG="-r"
+        ;;
+    "Linux")
+        DATE_ARG="-d"
+        ;;
+    *)
+        echo "Error: Unsupported platform"
+        exit 1;;
+esac
 
 TAG_DATE=0
 # Get latest tag name
 LatestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 if [ "$LatestTag" != "" ]; then
+    echo $LatestTag
     TAG_DATE=`git log -1 --format=%ct $LatestTag`
 fi
 
 
 # date -d @$TAG_DATE
-LastestTagDate=`date -r $TAG_DATE`
+LastestTagDate=`date $DATE_ARG $TAG_DATE`
 echo "LastestTag Date:"$LastestTagDate
 
 git submodule foreach "
